0aa155bb73bc5c0a09551cef489b0daf74b5aa42
Alexey Logachev <apelsin.alexey@gmail.com>
2023-12-21 14:29:17 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 1: Задайте значения M и N. Напишите
+// программу, которая выведет все натуральные числа
+// в промежутке от M до N. Использовать рекурсию, не
+// использовать циклы.
+
+void PrintNumbers(int m, int n)
+{
+    if (m == n) Console.Write($"{n}");
+    else
+    {
+        Console.Write($"{m} ");
+        PrintNumbers(m+1, n);
+    }
+}
+
+Console.WriteLine("Input your M: ");
+int user_M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input your N: ");
+int user_N = Convert.ToInt32(Console.ReadLine());
+if (user_M < user_N) PrintNumbers(user_M, user_N);
+else PrintNumbers(user_N, user_M);
+
+// Задача 2: Напишите программу вычисления функции
+// Аккермана с помощью рекурсии. Даны два
+// неотрицательных числа m и n. 
+
